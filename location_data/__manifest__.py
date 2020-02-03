4892bc24646ac194fbf4617307006e19182e0b66
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-02-03 11:39:59 +0100

    Add minimal data to reproduce loading bug in Odoo 13.0
    Step to reproduce (Odoo 13.0):
    
    1. Create a new database
    2. Install the module stock
    3. Install the module 'location_data'
    
    Expected behavior:
    
    We have new locations "Test 1" and a location "Test 2" under "STOCK".
    
    Current behavior:
    
    "Test 1" and "Test 2" are named "STOCK" and have a comment "A comment".
    They have been updated with the same values as the
    stock.stock_location_stock location.
    
    Note: it happens only if the XML for 'stock.stock_location_stock'
    contains the "active" field.

---
+++ location_data/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Location Data',
+    'summary': 'Reproduce issue when loading locations',
+    'version': '13.0.1.0.0',
+    'category': 'Hidden',
+    'author': 'Camptocamp',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'data/stock_location_data.xml',
+    ],
+    'installable': True,
+}
